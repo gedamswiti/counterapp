2c18a08aab797a31d4b7a790d1a87fdd0419f0f0
gedamswiti <switigedam17@gmail.com>
2023-05-09 19:21:18 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import React,{useState , useEffect, useReducer} from 'react'; 
+import "./App.scss"; 
+const initialState = {count: 0}; 
+ 
+function reducer(state:{count: number}, action:{type: string}) { 
+  switch (action.type) { 
+    case 'increment': 
+      return {count: state.count + 1}; 
+    case 'decrement': 
+      return {count: state.count - 1}; 
+    default: 
+      throw new Error(); 
+  } 
+} 
+ 
+const App = () => { 
+  const [state, dispatch] = useReducer(reducer, initialState); 
+ 
+ return( 
+    <div className='width100'> 
+      <span> test health app </span> 
+    <> 
+      Count: {state.count} 
+      <br/> 
+      <br/> 
+ 
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button> 
+      <button onClick={() => dispatch({type: 'increment'})}>+</button> 
+    </> 
+      </div> 
+  ); 
+}; 
 export default App;
