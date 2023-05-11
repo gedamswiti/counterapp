9486236ee6fa2cd4f0224185df798199d3cc1413
gedamswiti <switigedam17@gmail.com>
2023-05-11 23:13:54 +0530

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,27 @@
-import React,{useState , useEffect, useReducer} from 'react'; 
-import "./App.scss"; 
-const initialState = {count: 0}; 
+import { useState } from 'react'; 
+import logo from './logo.svg'; 
+import './App.css'; 
  
-function reducer(state:{count: number}, action:{type: string}) { 
-  switch (action.type) { 
-    case 'increment': 
-      return {count: state.count + 1}; 
-    case 'decrement': 
-      return {count: state.count - 1}; 
-    default: 
-      throw new Error(); 
+function App() { 
+ 
+  const [details, setdetails] = useState(" First Name") 
+  function updatedetails()  
+  { 
+    setdetails("gedam")   
   } 
+   
+  return ( 
+    <div className="App"> 
+     {details} 
+     <br/> 
+     <br/> 
+     <button onClick={updatedetails}> 
+      Tap To See   
+     </button> 
+ 
+    </div> 
+  ) 
 } 
  
-const App = () => { 
-  const [state, dispatch] = useReducer(reducer, initialState); 
  
- return( 
-    <div className='width100'> 
-      <span> test health app </span> 
-    <> 
-      Count: {state.count} 
-      <br/> 
-      <br/> 
-      
- 
-      <button onClick={() => dispatch({type: 'decrement'})}>-</button> 
-      <button onClick={() => dispatch({type: 'increment'})}>+</button> 
- 
-<div> 
-
-<h1> add date of births</h1>
-</div>
-
-      <br></br>
-      <input type="date"/>
-
-
-      <br></br>
-      <input type="date"/>
-     </>
-      </div> 
-
-
-
-  
-  );
-}; 
 export default App;
